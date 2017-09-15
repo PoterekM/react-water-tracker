995edf23bbf095467187e100796dc3bb404a25c4
Michelle Poterek <poterekm@gmail.com>
2017-09-15 08:58:05 -0700

    create plant model

---
+++ src/models/PlantModel.js
@@ -0,0 +1,11 @@
+class Plant {
+  constructor(name, plantType, soilType, timeWatered, timeSinceWatered) {
+    this.name = name;
+    this.plantType = plantType;
+    this.soilType = soilType;
+    this.timeWatered = timeWatered;
+    this.timeSinceWatered = timeSinceWatered;
+  }
+
+}
+export default Plant;
