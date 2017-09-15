8cd498d26c0310d091b69af2500bd1cf4b865670
Michelle Poterek <poterekm@gmail.com>
2017-09-15 08:52:22 -0700

    generate componentt for a form to add a plant

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Header from "./Header";
+import Form from "./Form";
+
 
 function App(props){
   return (
     <div>
       <Header/>
+      <Form/>
     </div>
   );
 }
---
+++ src/components/Form.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+
+function Form(props){
+  return (
+    <div>
+      <p>Hello, this is where the form will be! And there will be a button to hide and show information</p>
+    </div>
+  );
+
+}
+
+export default Form;
